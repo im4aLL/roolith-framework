0876f3a91ed36b1e529f3c033b1dd205b53ca94e
im4aLL <me@habibhadi.com>
2022-04-21 01:46:44 -0400

    updated frontend workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const babelify = require('babelify');
 const source = require('vinyl-source-stream');
