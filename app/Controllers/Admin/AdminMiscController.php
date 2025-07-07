13cc56cb9587e24165f3d5783ea094d0f16e7d90
Hadi <me@habibhadi.com>
2025-07-06 23:47:56 -0600

    file manager request updated

--- app/Controllers/Admin/AdminMiscController.php
+++ app/Controllers/Admin/AdminMiscController.php
@@ -2,51 +2,25 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\Controller;
+use App\Core\Request;
 use App\Utils\FileManager;
 
 class AdminMiscController extends Controller
 {
-    public function fileManager()
+    /**
+     * Basic file manager page
+     *
+     * @return bool|string
+     */
+    public function fileManager(): bool|string
     {
         // Initialize file manager
         $fm = new FileManager();
         $fm->setBaseUrl(url('files/'));
         $currentPath = $fm->getCurrentPath();
-        $message = '';
 
         // Handle form submissions
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (isset($_POST['action'])) {
-                switch ($_POST['action']) {
-                    case 'create_folder':
-                        if (!empty($_POST['folder_name'])) {
-                            if ($fm->createDirectory($currentPath, $_POST['folder_name'])) {
-                                $message = 'Folder created successfully';
-                            } else {
-                                $message = 'Failed to create folder';
-                            }
-                        }
-                        break;
-
-                    case 'upload_file':
-                        if (isset($_FILES['file'])) {
-                            $result = $fm->uploadFile($currentPath, $_FILES['file']);
-                            $message = $result['message'];
-                        }
-                        break;
-
-                    case 'delete':
-                        if (!empty($_POST['item_path'])) {
-                            if ($fm->deleteItem($_POST['item_path'])) {
-                                $message = 'Item deleted successfully';
-                            } else {
-                                $message = 'Failed to delete item';
-                            }
-                        }
-                        break;
-                }
-            }
-        }
+        $message = $this->handleFileManagerFormSubmission($fm);
 
         // Get directory contents
         $contents = $fm->scanDirectory($currentPath);
@@ -60,5 +34,56 @@ class AdminMiscController extends Controller
         ];
 
         return $this->view('admin/misc/admin-file-manager', $data);
+    }
+
+    /**
+     * Handle form submission from file manager and return message
+     *
+     * @param FileManager $fileManager
+     * @return string
+     */
+    private function handleFileManagerFormSubmission(FileManager $fileManager): string
+    {
+        $currentPath = $fileManager->getCurrentPath();
+        $message = '';
+
+        if (Request::method() !== 'POST') {
+            return $message;
+        }
+
+        if (!Request::input('action')) {
+            return $message;
+        }
+
+        switch (Request::input('action')) {
+            case 'create_folder':
+                if (!empty(Request::input('folder_name'))) {
+                    if ($fileManager->createDirectory($currentPath, Request::input('folder_name'))) {
+                        $message = 'Folder created successfully';
+                    } else {
+                        $message = 'Failed to create folder';
+                    }
+                }
+                break;
+
+            case 'upload_file':
+                if (isset($_FILES['file'])) {
+                    $result = $fileManager->uploadFile($currentPath, $_FILES['file']);
+                    $message = $result['message'];
+                }
+                break;
+
+            case 'delete':
+                if (!empty(Request::input('item_path'))) {
+                    if ($fileManager->deleteItem(Request::input('item_path'))) {
+                        $message = 'Item deleted successfully';
+                    } else {
+                        $message = 'Failed to delete item';
+                    }
+                }
+                break;
+        }
+
+        return $message;
     }
 }
